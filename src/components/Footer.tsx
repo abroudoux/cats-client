89b68ded6473ee7314de84fff49e9cae0ece28b6
abroudoux <arthur.broudoux@gmail.com>
2023-11-22 00:35:21 +0100

    modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
-import { Button } from "./ui/button";
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { Button } from '@/components/ui/button';
+
 
 export default function Footer() {
 
     return (
         <footer className="w-full h-20 border-t-2 px-5 flex-row-center-between">
-            <p>© 2023-present @abroudoux</p>
-            <Button variant="outline"><a href="https://github.com/abroudoux/cats-client.git" target="_blank">Source Code</a></Button>
+            <p>© 2023-present <a href="https://github.com/abroudoux" target="_blank">@abroudoux</a></p>
+            <Button variant="outline"><a href="https://github.com/abroudoux/cats-client.git" target="_blank">Source Code<FontAwesomeIcon icon={faGithub} className="pl-2" /></a></Button>
         </footer>
     );
 };
