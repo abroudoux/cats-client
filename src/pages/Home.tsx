c3aa081f32b8064e5e8c39552cbf26f31a6b69b0
abroudoux <arthur.broudoux@gmail.com>
2023-11-25 00:53:31 +0100

    create catcard & model createnewcat

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import {
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+  } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+
+import { CardCat } from '@/components/CardCat';
+import { Button } from '@/components/ui/button';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 interface Cat {
 	id : string,
@@ -29,12 +44,50 @@ export default function Home() {
 
 		<section className="page">
 
-			<h1 className="text-3xl">Welcome {userName}, discover your cats :</h1>
+			<h1 className="text-3xl mb-4">Welcome {userName}, meet your (cutes) friends :</h1>
 			<ul>
 				{cats.map((cat) => {
-					return <li key={cat.id}>{cat.name} - {cat.color}</li>
+					return <CardCat id={cat.id} name={cat.name} color={cat.color} isAdopted={cat.isAdopted} />
 				})}
 			</ul>
+			<div className="flex-row-center-between gap-3">
+				<p className="text-lg">Add a new cat ?</p>
+				<Dialog>
+      				<DialogTrigger asChild>
+        				<Button variant="outline">Create</Button>
+      				</DialogTrigger>
+      				<DialogContent className="sm:max-w-[425px]">
+        				<DialogHeader>
+          					<DialogTitle>Create a new cat</DialogTitle>
+        				</DialogHeader>
+						<div className="grid gap-4 py-4">
+							<div className="grid grid-cols-4 items-center gap-4">
+								<Label htmlFor="name" className="text-right">Name</Label>
+								<Input id="name" placeholder="What's his name?" className="col-span-3" />
+							</div>
+							<div className="grid grid-cols-4 items-center gap-4">
+								<Label htmlFor="color" className="text-right">Color</Label>
+								<Input id="color" placeholder="Which color is it?" className="col-span-3" />
+							</div>
+							<div className="grid grid-cols-4 items-center gap-4">
+                            	<Label htmlFor="status" className="text-right">Status</Label>
+								<Select>
+									<SelectTrigger id="status">
+										<SelectValue placeholder="Select" />
+									</SelectTrigger>
+									<SelectContent position="popper">
+										<SelectItem value="true">Adopted</SelectItem>
+										<SelectItem value="false">Not Adopted</SelectItem>
+									</SelectContent>
+								</Select>
+                        	</div>
+						</div>
+        				<DialogFooter>
+          					<Button type="submit">Create</Button>
+        				</DialogFooter>
+      				</DialogContent>
+    			</Dialog>
+			</div>
 
 		</section>
   	);
