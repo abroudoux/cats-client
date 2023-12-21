85ed2ca9348bebe6d6a6337a8f92da821e0d2b5e
abroudoux <arthur.broudoux@gmail.com>
2023-12-21 11:14:17 +0100

    resolve conflicts add push monorepo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
     return (
         <header className="w-full h-20 border-b-[1px] px-5 bg-background fixed top-0 flex-row-center-between">
             <Link to="/">
-                <h1 className="font-semibold text-4xl">Cats API</h1>
+                <h1 className="font-semibold text-4xl">cats-api</h1>
             </Link>
             <div className="flex flex-row items-center gap-4">
                 <DropdownMenu>
